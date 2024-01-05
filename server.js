c7b31a2c76f0a4684502f3947df350c67b4137ed
andreassef <andreassef02@gmail.com>
2024-01-04 21:47:02 -0300

    updating the server module

--- server.js
+++ server.js
@@ -1,38 +1,19 @@
 import http from 'node:http';
-
-const users = []
+import { json } from './middleware.js';
+import { routes } from './routes.js';
 
 const server = http.createServer(async (req, res) => {
     const { method, url } = req
 
-    const buffers = []
+    await json(req, res);
 
-    for await (const chunck of req) {
-        buffers.push(chunck)
+    const route = routes.find(route => {
+        return route.method == method && route.path == url
+    })
+
+    if (route) {
+        return route.handler(req, res)
     }
-
-    try {
-        req.body = JSON.parse(Buffer.concat(buffers).toString())
-    } catch {
-        req.body = null
-    }
-
-    if (method == 'GET' && url == '/users') {
-        return res.setHeader('Content-type', 'application/json').end(JSON.stringify(users))
-    }
-
-    if (method == 'POST' && url == '/users') {
-        const { name, email } = req.body
-
-        users.push({
-            id:1,
-            name,
-            email,
-        })
-
-        return res.writeHead(201).end()
-    }
-
     return res.writeHead(404).end()
 })
 
